29b5fcd69ba95cec735e4c4315895b249a39dae3
Bohdan Sokyrko <bosokyrk@c2r12s1.42wolfsburg.de>
2025-03-27 20:26:52 +0100

    color square

--- main.c
+++ main.c
@@ -1,26 +1,88 @@
 #include "minilibx-linux/mlx.h"
 #include <stdlib.h>
 #include <stdio.h>
-#define SIZE 20
+#define SIZE 500
+#define RED 0x00000000
+#define SHIFT 1
+#define WIDTH 1600
+#define HEIGHT 1200
 
 typedef struct s_data{
     void *mlx;
     void *win;
 } t_data;
 
-int draw_something(int x, int y, t_data *data)
+// int *colorshift()
+// {
+//     int* color = 0x00445500;
+//     return((int *)color);
+// }
+int draw_square(int x_zero, int y_zero, t_data *data)
 {
+    static int colorshift;
     int points = SIZE * 4;
-    int i = 0 - (SIZE/2);
+    int i = 0;
     int tempsize = SIZE;
-    mlx_mouse_get_pos(data->mlx, data->win, &x, &y);
+    mlx_mouse_get_pos(data->mlx, data->win, &x_zero, &y_zero);
     mlx_clear_window(data->mlx, data->win);
-    while(points--)
+    while(i <= (SIZE/2)) //RB
     {
-        mlx_pixel_put(data->mlx, data->win, x + (SIZE/2), y + i, 0x00FF0000);
+        mlx_pixel_put(data->mlx, data->win, x_zero + (SIZE/2), y_zero + i, RED + (colorshift * SHIFT));
         i++;
+        colorshift++;
     }
-
+    i = 0;
+    while(i <= (SIZE/2)) //RT
+    {
+        mlx_pixel_put(data->mlx, data->win, x_zero + (SIZE/2), y_zero - i, RED + (colorshift * SHIFT));
+        i++;
+        colorshift++;
+    }
+    i = 0;
+    while(i <= (SIZE/2)) //BR
+    {
+        mlx_pixel_put(data->mlx, data->win, x_zero + i, y_zero + (SIZE/2), RED + (colorshift * SHIFT));
+        i++;
+        colorshift++;
+    }
+    i = 0;
+    while(i <= (SIZE/2)) //BL
+    {
+        mlx_pixel_put(data->mlx, data->win, x_zero - i, y_zero + (SIZE/2), RED + (colorshift * SHIFT));
+        i++;
+        colorshift++;
+    }
+    i = 0;
+    while(i <= (SIZE/2)) //TL
+    {
+        mlx_pixel_put(data->mlx, data->win, x_zero - i, y_zero - (SIZE/2), RED + (colorshift * SHIFT));
+        i++;
+        colorshift++;
+    }
+    i = 0;
+    while(i <= (SIZE/2)) //TR
+    {
+        mlx_pixel_put(data->mlx, data->win, x_zero + i, y_zero - (SIZE/2), RED + (colorshift * SHIFT));
+        i++;
+        colorshift++;
+    }
+    i = 0;
+    while(i <= (SIZE/2)) //LT
+    {
+        mlx_pixel_put(data->mlx, data->win, x_zero - (SIZE/2), y_zero - i, RED + (colorshift * SHIFT));
+        i++;
+        colorshift++;
+    }
+    i = 0;
+    while(i <= (SIZE/2)) //LB
+    {
+        mlx_pixel_put(data->mlx, data->win, x_zero - (SIZE/2), y_zero + i, RED + (colorshift * SHIFT));
+        i++;
+        colorshift++;
+    }
+    printf("%d\n", RED + (colorshift * SHIFT));
+    if(colorshift > 150000)
+        colorshift = -colorshift;
 }
 int close_x(t_data *data)
 {
@@ -30,7 +92,7 @@ int close_x(t_data *data)
 }
 int close_esc(int keycode, t_data *data)
 {
-    printf("%d \n", keycode); //-------------- what key
+    printf("%d\n", keycode); //-------------- what key
     if(keycode == 65307)
     {
         mlx_destroy_window(data->mlx, data->win);
@@ -42,14 +104,12 @@ int close_esc(int keycode, t_data *data)
 int main()
 {
     t_data data;
-    int x;
-    int y;
 
     data.mlx = mlx_init();
-    data.win = mlx_new_window(data.mlx, 800, 600, "MiniLibX Window"); 
+    data.win = mlx_new_window(data.mlx, WIDTH, HEIGHT, "MiniLibX Window"); 
     mlx_hook(data.win, 17, 0, close_x, &data);
     mlx_key_hook(data.win, close_esc, &data);
-    mlx_hook(data.win, 6, 1L<<6, draw_something, &data);
+    mlx_hook(data.win, 6, 1L<<6, draw_square, &data);
     mlx_loop(data.mlx);
     return 0;
 }
