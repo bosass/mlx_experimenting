7b7367dd7185a1e021cb9153bae3b2d321d86bfa
Bohdan Sokyrko <bosokyrk@c1r3s2.42wolfsburg.de>
2025-05-19 21:44:21 +0200

    working sqr

--- fractal.c
+++ fractal.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#define SIZE 500
+#define SIZE 50
 #define RED 0x00FF0000
 // #define SHIFT 1
 #define WIDTH 1600
@@ -49,80 +49,65 @@ int close_esc(int keycode, t_data *data)
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+    if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT)
+    {
+        dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+        *(unsigned int*)dst = color;
+    }
 }
 int draw_square(int x_zero, int y_zero, t_data *data)
 {
-    // t_data data;
-    // static int colorshift;
-    // int points = SIZE * 4;
-    // int tempsize = SIZE;
+    memset(data->addr, 0, data->line_length * HEIGHT + WIDTH * (data->bits_per_pixel / 8));
+    
     int i = 0;
-    // mlx_mouse_get_pos(data->mlx, data->win, &x_zero, &y_zero);
-    // mlx_destroy_image(data->mlx, data->img);
-    // mlx_clear_window(data->mlx, data->win);
-    memset(data->addr, 0, WIDTH * HEIGHT * (data->bits_per_pixel / 8));
     
     while(i <= (SIZE/2)) //RB
     {
         my_mlx_pixel_put(data, x_zero + (SIZE/2), y_zero + i, RED);
         i++;
-        // colorshift++;
     }
     i = 0;
     while(i <= (SIZE/2)) //RT
     {
         my_mlx_pixel_put(data, x_zero + (SIZE/2), y_zero - i, RED);
         i++;
-        // colorshift++;
     }
     i = 0;
     while(i <= (SIZE/2)) //BR
     {
         my_mlx_pixel_put(data, x_zero + i, y_zero + (SIZE/2), RED);
         i++;
-        // colorshift++;
     }
     i = 0;
     while(i <= (SIZE/2)) //BL
     {
         my_mlx_pixel_put(data, x_zero - i, y_zero + (SIZE/2), RED);
         i++;
-        // colorshift++;
     }
     i = 0;
     while(i <= (SIZE/2)) //TL
     {
         my_mlx_pixel_put(data, x_zero - i, y_zero - (SIZE/2), RED);
         i++;
-        // colorshift++;
     }
     i = 0;
     while(i <= (SIZE/2)) //TR
     {
         my_mlx_pixel_put(data, x_zero + i, y_zero - (SIZE/2), RED);
         i++;
-        // colorshift++;
     }
     i = 0;
     while(i <= (SIZE/2)) //LT
     {
         my_mlx_pixel_put(data, x_zero - (SIZE/2), y_zero - i, RED);
         i++;
-        // colorshift++;
     }
     i = 0;
     while(i <= (SIZE/2)) //LB
     {
         my_mlx_pixel_put(data, x_zero - (SIZE/2), y_zero + i, RED);
         i++;
-        // colorshift++;
     }
-    // printf("%d\n", RED + (colorshift * SHIFT));
-    // if(colorshift > 150000)
-    //     colorshift = -colorshift;
     mlx_put_image_to_window(data->mlx, data->win, data->img, 0, 0);
     return (0);
 }
@@ -135,15 +120,12 @@ int main()
 	data.img = mlx_new_image(data.mlx, WIDTH, HEIGHT);
 	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
     
-    // // my_mlx_pixel_put(&data, 5, 5, 0x00FF0000);
-    draw_square(WIDTH/2, HEIGHT/2, &data);
+    // draw_square(WIDTH/2, HEIGHT/2, &data);
     mlx_hook(data.win, 6, 1L<<6, draw_square, &data);
 
     mlx_hook(data.win, 17, 0, close_x, &data);
     mlx_key_hook(data.win, close_esc, &data);
 
 	mlx_loop(data.mlx);
-    // mlx_hook(data.win, 6, 1L<<6, draw_square, &data);
-    // mlx_loop(data.mlx);
     return 0;
 }
